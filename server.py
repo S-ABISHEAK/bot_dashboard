47e89e9289e573eadd82699100e1411feabeecae
S ABISHEAK <sakabisheak@gmail.com>
2026-09-16 20:24:01 +0530

    Update server to use environment variable for port

--- server.py
+++ server.py
@@ -319,5 +319,7 @@ app.mount("/static", StaticFiles(directory=STATIC), name="static")
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="warning")
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="warning")
